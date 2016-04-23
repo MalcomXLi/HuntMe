fe36e79aa6bcc14cfde2e947edeb37918df35cf3
Maidi Lin <m39lin@uwaterloo.ca>
2016-04-23 02:56:13 -0400

    updated all endpoints

--- server.js
+++ server.js
@@ -19,37 +19,53 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res)  {
   db.collection('team').find().toArray( function(err, result) {
-    if (err) return console.log(err);
-    console.log(result);
-    res.send(result);
+    if (err) {
+        console.log(err);
+        res.status(404).send(err);
+    } else {   
+        //console.log(result);
+        res.status(200).send(result);
+    }
   });
 });
 
 app.post('/create', function(req, res)  {
   db.collection('team').save(req.body, function(err, result) {
-    if (err) return console.log(err);
-    console.log('saved to database');
+    if (err) {
+        console.log(err);
+        res.status(400).send(err);
+    } else {
+        console.log('saved to database');
+        //console.log(result);
+        res.status(201).send(result);
+    }
   });
 });
 
 app.put('/update', function(req, res)  {
-  db.collection('team').findOneAndUpdate({name: 'Yoda'}, {
+  db.collection('team').findOneAndUpdate({name: req.body.name}, {
     $set: {
-      name: req.body.name,
-      quote: req.body.quote
+      latitude: req.body.latitude,
+      longitude: req.body.longitude
     }
   }, {
     sort: {_id: -1},
     upsert: true
   }, function(err, result)  {
-    if (err) return res.send(err);
-    res.send(result);
+    if (err) {
+        res.status(400).send(err);
+    } else {
+        res.status(200).send(result);
+    }
   });
 });
 
-app.delete('/remove', function(req, res)  {
-  db.collection('quotes').findOneAndDelete({name: req.body.name}, function(err, result)  {
-    if (err) return res.send(500, err);
-    res.send('A darth vadar quote got deleted');
+app.delete('/delete', function(req, res)  {
+  db.collection('team').findOneAndDelete({name: req.body.name}, function(err, result)  {
+    if (err) {
+        res.status(500).send(err);
+    } else {
+        res.status(200).send('A darth vadar quote got deleted');
+    }
   });
 });
